b6941dc2d509dd79afb4112fb6701d02f68fe20c
Rick South <rick@natamus.com>
2024-07-28 07:28:45 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/hoetweaks/util/Reference.java
+++ Common/src/main/java/com/natamus/hoetweaks/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.hoetweaks.util;
 public class Reference {
 	public static final String MOD_ID = "hoetweaks";
 	public static final String NAME = "Hoe Tweaks";
-	public static final String VERSION = "3.4";
+	public static final String VERSION = "3.5";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }
--- NeoForge/src/main/java/com/natamus/hoetweaks/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/hoetweaks/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.hoetweaks;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.hoetweaks.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.hoetweaks.neoforge.events.NeoForgeHoeEvent;
 import com.natamus.hoetweaks.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
