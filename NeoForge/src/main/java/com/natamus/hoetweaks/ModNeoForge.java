0dfb2e5c43d31478458a051cba2346e17c3d76f3
Rick South <rick@natamus.com>
2024-10-24 19:54:14 +0200

    Hoe Tweaks source code for 1.21.2.

---
+++ NeoForge/src/main/java/com/natamus/hoetweaks/ModNeoForge.java
@@ -0,0 +1,39 @@
+package com.natamus.hoetweaks;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.hoetweaks.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.hoetweaks.neoforge.events.NeoForgeHoeEvent;
+import com.natamus.hoetweaks.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeHoeEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
