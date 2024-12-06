d3cb132c4e86e9c0bdc83844077507a6b8fdf618
Rick South <rick@natamus.com>
2024-12-06 13:27:20 +0100

    Hoe Tweaks source code for 1.21.4.

---
+++ Common/src/main/java/com/natamus/hoetweaks/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.hoetweaks;
+
+import com.natamus.hoetweaks.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
