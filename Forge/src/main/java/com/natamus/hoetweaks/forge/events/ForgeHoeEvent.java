5f7ffb676c3cfc9f3f5e84b18b036f457deac990
Rick South <rick@natamus.com>
2025-02-12 23:29:07 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/hoetweaks/forge/events/ForgeHoeEvent.java
+++ Forge/src/main/java/com/natamus/hoetweaks/forge/events/ForgeHoeEvent.java
@@ -5,19 +5,17 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeHoeEvent {
 	@SubscribeEvent
-	public void onHoeRightClickBlock(PlayerInteractEvent.RightClickBlock e) {
+	public static void onHoeRightClickBlock(PlayerInteractEvent.RightClickBlock e) {
 		if (!HoeEvent.onHoeRightClickBlock(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), null)) {
 			e.setCanceled(true);
 		}
 	}
 	
 	@SubscribeEvent
-	public void onHarvestBreakSpeed(PlayerEvent.BreakSpeed e) {
+	public static void onHarvestBreakSpeed(PlayerEvent.BreakSpeed e) {
 		Player player = e.getEntity();
 		float originalSpeed = e.getOriginalSpeed();
 		float newSpeed = HoeEvent.onHarvestBreakSpeed(player.level(), player, originalSpeed, e.getState());
