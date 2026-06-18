e0e44625dc506a271c80e64dc9c5dacf3b4f910b
Rick South <rick@natamus.com>
2026-06-18 23:46:26 +0200

    Updated Hoe Tweaks to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/hoetweaks/util/Reference.java
+++ Common/src/main/java/com/natamus/hoetweaks/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "hoetweaks";
 	public static final String NAME = "Hoe Tweaks";
 	public static final String VERSION = "3.6";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
